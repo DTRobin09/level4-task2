b48a8402d6c6d81a106609b66bee3efbabc31bc8
Robin <dtr.robin09@gmail.com>
2020-10-19 03:06:34 +0200

    Finished level

--- app/src/main/java/com/example/madlevel4task2/view/GameHistoryFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/view/GameHistoryFragment.kt
@@ -1,15 +1,22 @@
 package com.example.madlevel4task2.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.madlevel4task2.R
 import com.example.madlevel4task2.model.Game
 import com.example.madlevel4task2.repository.GameRepository
+import kotlinx.android.synthetic.main.fragment_game_history.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -21,10 +28,13 @@ class GameHistoryFragment : Fragment() {
     private var gamesList = arrayListOf<Game>()
     private var gameHistoryAdapter = GameHistoryAdapter(gamesList)
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game_history, container, false)
     }
@@ -32,5 +42,53 @@ class GameHistoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        gameRepository = GameRepository(requireContext())
+        initViews()
+        getGamesFromDatabase()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            onBackPressed();
+        }
+
+        return when (item.itemId) {
+            R.id.action_clear -> {
+                deleteAllGames()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun onBackPressed() {
+        findNavController().popBackStack()
+    }
+
+    private fun deleteAllGames() {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.IO) {
+                gameRepository.deleteAllGames()
+            }
+            getGamesFromDatabase()
+            gameHistoryAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun initViews() {
+        rvGameHistory.layoutManager = LinearLayoutManager(context)
+        rvGameHistory.adapter = gameHistoryAdapter
+        gameHistoryAdapter.notifyDataSetChanged()
+    }
+
+    private fun getGamesFromDatabase() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val games = withContext(Dispatchers.IO) {
+                gameRepository.getAllGames()
+            }
+            this@GameHistoryFragment.gamesList.clear()
+            this@GameHistoryFragment.gamesList.addAll(games)
+            gameHistoryAdapter.notifyDataSetChanged()
+        }
     }
 }
